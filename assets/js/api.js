6b6e6a7dee35342c8fd8f4c75536af7a0eafd117
RafaelSantos6 <rafaeljlle91@gmail.com>
2025-11-21 17:03:46 -0300

    add a função de busca por categoria

--- assets/js/api.js
+++ assets/js/api.js
@@ -93,3 +93,39 @@ async function buscarReceita() {
     console.error("Erro ao buscar receita:", error);
   }
 }
+
+// BUSCA POR CATEGORIA // 
+
+async function buscarReceitas(categoria) {
+    const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
+
+    document.getElementById("resultado").innerHTML = "<p>Carregando receitas...</p>";
+
+    try {
+        const resposta = await fetch(url);
+        const dados = await resposta.json();
+
+        if (!dados.meals) {
+            document.getElementById("resultado").innerHTML =
+                "<p>Nenhuma receita encontrada.</p>";
+            return;
+        }
+
+        const html = dados.meals
+            .map(meal => `
+                <div class="receita-card">
+                    <img src="${meal.strMealThumb}">
+                    <h3>${meal.strMeal}</h3>
+                </div>
+            `)
+            .join("");
+
+        document.getElementById("resultado").innerHTML = `
+            <h2>Receitas da categoria: ${categoria}</h2>
+            <div class="grid-receitas">${html}</div>
+        `;
+    } catch (e) {
+        document.getElementById("resultado").innerHTML =
+            "<p>Erro ao carregar receitas.</p>";
+    }
+}
